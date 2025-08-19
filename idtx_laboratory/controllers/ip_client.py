b3008a61989a8238da3f15378ea57c920ebc8d07
JPC <jcf@perucodex.com>
2025-08-19 15:32:20 -0500

    Balanza

---
+++ idtx_laboratory/controllers/ip_client.py
@@ -0,0 +1,12 @@
+from odoo import http
+
+class MyController(http.Controller):
+    @http.route('/get_ip', type='http', auth='public')
+    def get_ip(self, **kw):
+        request = http.request
+        ip = request.httprequest.headers.get('X-Forwarded-For')
+        if not ip:
+            ip = request.httprequest.headers.get('X-Real-IP')
+        if not ip:
+            ip = request.httprequest.remote_addr  # fallback
+        return f"IP del cliente: {ip}"
