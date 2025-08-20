c88083df16f49d2f366ecce43a4172ba96145740
JPC <jcf@perucodex.com>
2025-08-20 10:30:36 -0500

    Balanza

--- idtx_laboratory/controllers/ip_cliente.py
+++ idtx_laboratory/controllers/ip_cliente.py
@@ -8,6 +8,10 @@ class MyController(http.Controller):
     def get_ip(self, **kwargs):
         # Apache ya pone la IP real en REMOTE_ADDR
         client_ip = request.httprequest.environ.get('REMOTE_ADDR')
-        if not client_ip:
-            return "No se recibió la IP del cliente"
-        return f"La IP del cliente es: {client_ip}"
+        headers = dict(request.httprequest.headers)
+        remote_addr = request.httprequest.environ.get('REMOTE_ADDR')
+        xff = request.httprequest.headers.get('X-Forwarded-For')
+        return f"REMOTE_ADDR={remote_addr} | X-Forwarded-For={xff} | headers={headers}"
+        # if not client_ip:
+        #     return "No se recibió la IP del cliente"
+        # return f"La IP del cliente es: {client_ip}"
