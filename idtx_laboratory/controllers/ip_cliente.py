8c72cb0a2c3496ce56fe0bbd9211057a267f8f04
JPC <jcf@perucodex.com>
2025-08-20 10:28:31 -0500

    Balanza

--- idtx_laboratory/controllers/ip_cliente.py
+++ idtx_laboratory/controllers/ip_cliente.py
@@ -1,10 +1,13 @@
+# controllers/ip_controller.py
 from odoo import http
 from odoo.http import request
 
 class MyController(http.Controller):
 
     @http.route('/ip_client', type='http', auth='public', methods=['GET'], csrf=False)
-    def test(self, **kwargs):
-        # Capturar la IP del cliente
-        client_ip = request.httprequest.headers.get('X-Forwarded-For', request.httprequest.remote_addr)
+    def get_ip(self, **kwargs):
+        # Apache ya pone la IP real en REMOTE_ADDR
+        client_ip = request.httprequest.environ.get('REMOTE_ADDR')
+        if not client_ip:
+            return "No se recibió la IP del cliente"
         return f"La IP del cliente es: {client_ip}"
