d4edc82e4f4e2cf4c84f07d98cc2f0263100f2cb
Rdomarcelo <rx_marcelo@yahoo.com>
2026-05-15 17:13:26 -0500

    [FIX] idtx_pos_sale_idetex: POS receipt A4 layout and PDF download logic
    - Fixed OrderDisplay Owl crash by applying CSS instead of XML replacement
    
    - Expanded POS receipt container for A4 format
    
    - Added 'Imprimir Factura Boleta' button to ReceiptScreen
    
    - Modified Invoice buttons to download PDF natively instead of opening in a new tab

--- idtx_pos_sale_idetex/static/src/app/screens/receipt_screen/receipt_screen_patch.js
+++ idtx_pos_sale_idetex/static/src/app/screens/receipt_screen/receipt_screen_patch.js
@@ -3,6 +3,13 @@
 import { ReceiptScreen } from "@point_of_sale/app/screens/receipt_screen/receipt_screen";
 import { patch } from "@web/core/utils/patch";
 import { useState } from "@odoo/owl";
+import OrderPaymentValidation from "@point_of_sale/app/utils/order_payment_validation";
+
+patch(OrderPaymentValidation.prototype, {
+    shouldDownloadInvoice() {
+        return false;
+    }
+});
 
 patch(ReceiptScreen.prototype, {
     setup() {
@@ -15,5 +22,12 @@ patch(ReceiptScreen.prototype, {
 
     togglePrintDetailed() {
         this.receiptState.printDetailed = !this.receiptState.printDetailed;
+    },
+
+    async printInvoice() {
+        const accountMoveId = this.currentOrder.raw.account_move;
+        if (accountMoveId) {
+            await this.pos.env.services.account_move.downloadPdf(accountMoveId);
+        }
     }
 });
--- idtx_pos_sale_idetex/static/src/app/screens/ticket_screen/ticket_screen_patch.js
+++ idtx_pos_sale_idetex/static/src/app/screens/ticket_screen/ticket_screen_patch.js
@@ -2,14 +2,6 @@
 
 import { TicketScreen } from "@point_of_sale/app/screens/ticket_screen/ticket_screen";
 import { patch } from "@web/core/utils/patch";
-import { AccountMoveService } from "@account/services/account_move_service";
-
-patch(AccountMoveService.prototype, {
-    async downloadPdf(accountMoveId, target = "new") {
-        const url = `/report/pdf/account.account_invoices/${accountMoveId}`;
-        window.open(url, "_blank");
-    }
-});
 
 patch(TicketScreen.prototype, {
     setup() {
@@ -32,19 +24,13 @@ patch(TicketScreen.prototype, {
             }
 
             if (accountMoveId) {
-                const action = await this.pos.data.call("account.move", "preview_invoice", [accountMoveId]);
-                if (action && action.url) {
-                    window.open(action.url, "_blank");
-                }
+                await this.pos.env.services.account_move.downloadPdf(accountMoveId);
             } else {
                 // Si la orden no tiene factura asociada, intentar generarla
                 await this.pos.data.call("pos.order", "action_pos_order_invoice", [order.id]);
                 const orders = await this.pos.data.loadServerOrders([["id", "=", order.id]]);
                 if (orders && orders.length > 0 && orders[0].raw.account_move) {
-                    const action = await this.pos.data.call("account.move", "preview_invoice", [orders[0].raw.account_move]);
-                    if (action && action.url) {
-                        window.open(action.url, "_blank");
-                    }
+                    await this.pos.env.services.account_move.downloadPdf(orders[0].raw.account_move);
                 }
             }
         } catch (error) {
