c0e44dd391c29fd3b1d2211e01a7ad57b8f766df
perucodex <101416897+perucodex@users.noreply.github.com>
2026-09-09 15:26:11 -0500

    Add unique code constraint to product models

--- idtx_product_development/models/product_code_system.py
+++ idtx_product_development/models/product_code_system.py
@@ -7,22 +7,26 @@ class ProductCodeSystem(models.Model):
     code = fields.Char('Code')
     name = fields.Char('Name')
 
-    _code_uniq = models.Constraint('unique(code)', 'Code must be unique!')
-
 class ProductFamily(models.Model):
     _name = 'product.family'
     _inherit = 'product.code.system'
     _description = 'Product Family'
+
+    _code_uniq = models.Constraint('unique(code)', 'Code must be unique!')
 
 class ProductTitle(models.Model):
     _name = 'product.title'
     _inherit = 'product.code.system'
     _description = 'Product Title'
 
+    _code_uniq = models.Constraint('unique(code)', 'Code must be unique!')
+
 class ProductFiber(models.Model):
     _name = 'product.fiber'
     _inherit = 'product.code.system'
     _description = 'Product Fiber'
+
+    _code_uniq = models.Constraint('unique(code)', 'Code must be unique!')
 
 class ProductGauge(models.Model):
     _name = 'product.gauge'
@@ -38,7 +42,11 @@ class ProductAppearance(models.Model):
     _inherit = 'product.code.system'
     _description = 'Product Appearance'
 
+    _code_uniq = models.Constraint('unique(code)', 'Code must be unique!')
+
 class LigamentType(models.Model):
     _name = 'ligament.type'
     _inherit = 'product.code.system'
     _description = 'Ligament Type'
+
+    _code_uniq = models.Constraint('unique(code)', 'Code must be unique!')
